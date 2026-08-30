b2e2b171ed4db2d440bfb42537281aef465c75a0
dk444312 <Kandododesire0@gmail.com>
2026-08-30 10:39:06 +0200

    Replace hardcoded SUPABASE_ANON_KEY with env variable
    Remove hardcoded Supabase anon key and use environment variable instead.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,7 +7,7 @@ export const SUPABASE_URL =
 
 export const SUPABASE_ANON_KEY = 
   import.meta.env.VITE_SUPABASE_ANON_KEY || 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJncWRuc3VubGxueWxlY3R3bmxrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Nzk1NjIzMCwiZXhwIjoyMTAzNTMyMjMwfQ.wZqnTDOFeURGNvYzWKFbTk-r97wvlGfINJPYZeEUneY';
+  '';
 
 // Initialized Supabase Client instance
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
